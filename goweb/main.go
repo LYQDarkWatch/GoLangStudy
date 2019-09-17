d19411990f19950be4baea44eb204af8f578e5f0
liuyuqi <961716511@qq.com>
2019-09-17 16:59:36 +0800

    9.17 updates

--- goweb/main.go
+++ goweb/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -8,6 +9,14 @@ import (
 )
 
 type MyMux struct {
+}
+type Auth struct {
+	Username string `json:"user"`
+	Pwd      string `json:"password"`
+}
+type Resp struct {
+	Code string `json:"code"`
+	Msg  string `json:"msg"`
 }
 
 var github = map[string]string{}
@@ -21,7 +30,10 @@ func (p *MyMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		ClearMap(w, r)
 		return
 	}
-
+	if r.URL.Path == "/login" {
+		Login(w, r)
+		return
+	}
 }
 func about(w http.ResponseWriter, r *http.Request) string {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
@@ -35,6 +47,33 @@ func about(w http.ResponseWriter, r *http.Request) string {
 	fmt.Println(github)
 	fmt.Fprint(w, github["https://api.github.com/search/repositories?q=stars:%3E=500&sort=stars&order=desc"])
 	return b
+}
+func Login(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Add("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("content-type", "application/json")
+	if r.Method == "POST" {
+		s, _ := ioutil.ReadAll(r.Body)
+		a := string(s)
+		login(s)
+		fmt.Print(a)
+	}
+}
+func login(userInfo []byte) {
+	var user Auth
+	var result Resp
+	json.Unmarshal(userInfo, &user)
+	if user.Username == "admin" && user.Pwd == "123456" {
+		result.Code = "200"
+		result.Msg = "登录成功"
+		fmt.Print(result.Msg)
+	} else {
+		result.Code = "401"
+		result.Msg = "账户名或密码错误"
+		fmt.Print(result.Msg)
+
+	}
+
 }
 func Start() {
 	mux := &MyMux{}
