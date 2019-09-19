5d34ae3267e573ece094aff451e87c8ea15b3a93
LYQDarkWatch <44162227+LYQDarkWatch@users.noreply.github.com>
2019-09-19 17:13:13 +0800

    Add files via upload

--- goweb/main.go
+++ goweb/main.go
@@ -9,60 +9,98 @@ import (
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
+	"github.com/garyburd/redigo/redis"
 )
 
-type jwtCustomClaims struct {
-	jwt.StandardClaims
+const (
+	SecretKey = "I have login"
+)
 
-	// 追加自己需要的信息
-	Uid   uint `json:"uid"`
-	Admin bool `json:"admin"`
+type UserCredentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+// type User struct {
+// 	ID       int    `json:"id"`
+// 	Name     string `json:"name"`
+// 	Username string `json:"username"`
+// 	Password string `json:"password"`
+// }
+
+type Response struct {
+	Data string `json:"data"`
 }
 
 type Token struct {
 	Token string `json:"token"`
 }
-type MyMux struct {
+
+func StartServer() {
+
+	http.HandleFunc("/login", LoginHandler)
+
+	http.HandleFunc("/about", about)
+	http.HandleFunc("/clear", ClearMap)
+	log.Println("Now listening...")
+	http.ListenAndServe(":8080", nil)
 }
-type Auth struct {
-	Username string `json:"user"`
-	Pwd      string `json:"password"`
+
+func main() {
+	StartServer()
 }
-type Resp struct {
-	Code string `json:"code"`
-	Msg  string `json:"msg"`
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Add("Access-Control-Allow-Headers", "Content-Type")
+	// var u *User = new(User)
+	var user UserCredentials
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusForbidden)
+		fmt.Fprint(w, "Error in request")
+		return
+	}
+
+	if user.Username == "admin" && user.Password == "123456" {
+		fmt.Println("欢迎用户: " + user.Username + " 登录成功")
+		token := jwt.New(jwt.SigningMethodHS256)
+		claims := make(jwt.MapClaims)
+
+		claims["exp"] = time.Now().Add(time.Hour * time.Duration(1)).Unix()
+		claims["iat"] = time.Now().Unix()
+		// claims["id"] = u.ID
+		claims["userName"] = user.Username
+		claims["password"] = user.Password
+		token.Claims = claims
+
+		fmt.Println(claims)
+
+		tokenString, _ := token.SignedString([]byte(SecretKey))
+		fmt.Println(tokenString)
+		response := Token{tokenString}
+		JsonResponse(response, w)
+
+	} else {
+		fmt.Fprintf(w, `{"code":401}`)
+		fmt.Println("账号或密码错误")
+
+	}
+
+}
+func JsonResponse(response interface{}, w http.ResponseWriter) {
+	json, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(json)
 }
 
 var github = map[string]string{}
 
-func CreateToken(SecretKey []byte, issuer string, Uid uint, isAdmin bool) (tokenString string, err error) {
-	claims := &jwtCustomClaims{
-		jwt.StandardClaims{
-			ExpiresAt: int64(time.Now().Add(time.Hour * 72).Unix()),
-			Issuer:    issuer,
-		},
-		Uid,
-		isAdmin,
-	}
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	tokenString, err = token.SignedString(SecretKey)
-	return
-}
-
-func (p *MyMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/about" {
-		about(w, r)
-		return
-	}
-	if r.URL.Path == "/clear" {
-		ClearMap(w, r)
-		return
-	}
-	if r.URL.Path == "/login" {
-		Login(w, r)
-		return
-	}
-}
 func about(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Add("Access-Control-Allow-Headers", "Content-Type")
@@ -74,46 +112,40 @@ func about(w http.ResponseWriter, r *http.Request) {
 	github["https://api.github.com/search/repositories?q=stars:%3E=500&sort=stars&order=desc"] = b
 	fmt.Fprint(w, github["https://api.github.com/search/repositories?q=stars:%3E=500&sort=stars&order=desc"])
 }
-func Login(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Add("Access-Control-Allow-Headers", "Content-Type")
-	w.Header().Set("content-type", "application/json")
-	if r.Method == "POST" {
-		var user Auth
-		s, _ := ioutil.ReadAll(r.Body)
-		json.Unmarshal([]byte(s), &user)
-		if user.Username == "admin" && user.Pwd == "123456" {
-			keyInfo := "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()"
-			var dataStr = string(s)
-			data := jwt.StandardClaims{Subject: dataStr, ExpiresAt: time.Now().Unix() - 1000}
-			tokenInfo := jwt.NewWithClaims(jwt.SigningMethodHS256, data)
-			tokenStr, _ := tokenInfo.SignedString([]byte(keyInfo))
-			fmt.Println("myToken is: ", tokenStr)
-			tokenInfo, _ = jwt.Parse(tokenStr, func(token *jwt.Token) (i interface{}, e error) {
-				return keyInfo, nil
-			})
-
-			fmt.Fprintf(w, `{"code":200}`)
-			fmt.Println("欢迎用户: " + user.Username + " 登录成功")
-		} else {
-			fmt.Fprintf(w, `{"code":401}`)
-			fmt.Println("账号或密码错误")
-		}
-
-	}
-}
-func Start() {
-	mux := &MyMux{}
-	err := http.ListenAndServe(":8080", mux)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
-}
 func ClearMap(w http.ResponseWriter, r *http.Request) {
 	delete(github, "https://api.github.com/search/repositories?q=stars:%3E=500&sort=stars&order=desc")
 	fmt.Fprint(w, github["https://api.github.com/search/repositories?q=stars:%3E=500&sort=stars&order=desc"])
 }
-func main() {
 
-	Start()
+const (
+	RedisURL            = "redis://*****:6379"
+	redisMaxIdle        = 3   //最大空闲连接数
+	redisIdleTimeoutSec = 240 //最大空闲连接时间
+	RedisPassword       = "*****"
+)
+
+// NewRedisPool 返回redis连接池
+func NewRedisPool(redisURL string) *redis.Pool {
+	return &redis.Pool{
+		MaxIdle:     redisMaxIdle,
+		IdleTimeout: redisIdleTimeoutSec * time.Second,
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.DialURL(redisURL)
+			if err != nil {
+				return nil, fmt.Errorf("redis connection error: %s", err)
+			}
+			//验证redis密码
+			if _, authErr := c.Do("AUTH", RedisPassword); authErr != nil {
+				return nil, fmt.Errorf("redis auth password error: %s", authErr)
+			}
+			return c, err
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			_, err := c.Do("PING")
+			if err != nil {
+				return fmt.Errorf("ping redis error: %s", err)
+			}
+			return nil
+		},
+	}
 }
